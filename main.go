3d02544ba64fdad9a1dee30393fc32cfc3cedeb3
Karl Daniel <kkjdaniel@yahoo.co.uk>
2025-09-16 02:48:25 +0100

    chore: version bump

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 func createMCPServer() *server.MCPServer {
 	s := server.NewMCPServer(
 		"BGG MCP",
-		"1.3.2",
+		"1.4.0",
 		server.WithResourceCapabilities(true, true),
 		server.WithPromptCapabilities(true),
 		server.WithLogging(),
